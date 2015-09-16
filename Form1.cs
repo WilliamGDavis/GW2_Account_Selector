f20bb900c727aab9db0230d4b51844396e824878
WilliamGDavis <will@willdavis.net>
2015-09-16 14:26:18 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,14 +15,18 @@ namespace GuildWars2_Launcher
 {
     public partial class Form1 : Form
     {
+        public DataSet ds;
+        public DataTable dt;
+        public string XmlDirectory;
+        public int currRow;
+
         public Form1()
         {
             InitializeComponent();
+            this.ds = new DataSet();
+            this.dt = null;
+            this.XmlDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\My Documents\\GW2Data.xml";
         }
-
-        DataSet ds = new DataSet();
-        DataTable dt = null;
-        string XmlDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\My Documents\\GW2Data.xml";
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -39,13 +43,38 @@ namespace GuildWars2_Launcher
             PopulateData();
         }
 
-        private void PopulateData()
+        private void btn_Edit_Click(object sender, EventArgs e)
         {
-            XmlReader xmlreader;
-            int currRow = 0;
+            int currentRowIndex;
 
             //Get the currently selected row for later recall
-            if (dgv_Accounts.SelectedRows.Count > 0) { currRow = dgv_Accounts.SelectedRows[0].Index; }
+            if (dgv_Accounts.SelectedRows.Count > 0) { currentRowIndex = dgv_Accounts.SelectedRows[0].Index; } else { return; }
+
+            //Instantiate a new "Edit_Account" object and pass in the variables and parent form
+            Edit_Account edit_account = new Edit_Account(this);
+            edit_account.dtRow = currentRowIndex;
+            edit_account.acctName = this.dt.Rows[currentRowIndex][0].ToString();
+            edit_account.email = this.dt.Rows[currentRowIndex][1].ToString();
+            edit_account.password = this.dt.Rows[currentRowIndex][2].ToString();
+            edit_account.server = this.dt.Rows[currentRowIndex][3].ToString();
+            edit_account.prodKey = this.dt.Rows[currentRowIndex][4].ToString();
+            edit_account.apiKey = null;
+            edit_account.ShowDialog();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            //Create the Add_Account object and pass in the parent form (this)
+            Add_Account Add_Account = new Add_Account(this);
+            Add_Account.Show();
+        }
+
+        public void PopulateData()
+        {
+            XmlReader xmlreader;
+
+            //Get the currently selected row for later recall
+            if (dgv_Accounts.SelectedRows.Count > 0 && this.currRow != 0) { currRow = dgv_Accounts.SelectedRows[0].Index; }
 
             //Clear out the data table and data grid
             if (this.ds.Tables.Count > 0 && this.ds.Tables[0] != null) { this.ds.Tables[0].Clear(); }
@@ -68,7 +97,10 @@ namespace GuildWars2_Launcher
             buildDataGridView(dgv_Accounts);
 
             //Re-Select the row that was selected before the refresh command
-            dgv_Accounts.Rows[currRow].Selected = true;
+            if (currRow != 0) { dgv_Accounts.Rows[currRow].Selected = true; }
+
+            //Auto-Scroll to the selected row in the datagrid (If there is a scrollbar)
+            dgv_Accounts.FirstDisplayedScrollingRowIndex = currRow;
 
             //Clean and dispose of the xmlreader
             destroyXmlReader(xmlreader);
@@ -100,7 +132,7 @@ namespace GuildWars2_Launcher
                 foreach (DataRow dr in this.dt.Rows)
                 {
                     //Capture the data from the datatable
-                    string acctNumber = dr["Number"].ToString();
+                    int acctNumber = (this.dt.Rows.IndexOf(dr) + 1); //Generate an account number
                     string acctName = dr["Name"].ToString();
                     string acctEmail = dr["Email"].ToString();
                     string acctPassword = dr["Password"].ToString();
@@ -110,7 +142,7 @@ namespace GuildWars2_Launcher
 
                     //Format the datagrid rows and add them to the datagrid
                     string column1 = acctName + Environment.NewLine + server + Environment.NewLine + acctEmail + Environment.NewLine + prodKey;
-                    string[] row = new string[] { acctNumber, column1, lastLogin };
+                    string[] row = new string[] { acctNumber.ToString(), column1, lastLogin };
                     dgv_Accounts.Rows.Add(row);
                 }
             }
@@ -125,9 +157,9 @@ namespace GuildWars2_Launcher
             if (dgv_Accounts.SelectedRows.Count > 0) { currentRowIndex = dgv_Accounts.SelectedRows[0].Index; } else { return; }
 
             //Retrieve data from the datatable, not the datagrid.  It allows us to get the 'Hidden' values
-            string email = this.dt.Rows[currentRowIndex][2].ToString();
-            string password = this.dt.Rows[currentRowIndex][3].ToString();
-            string lastLogin = this.dt.Rows[currentRowIndex][6].ToString();
+            string email = this.dt.Rows[currentRowIndex][1].ToString();
+            string password = this.dt.Rows[currentRowIndex][2].ToString();
+            string lastLogin = this.dt.Rows[currentRowIndex][5].ToString();
             string updatedDate = DateTime.Now.ToString();
 
             //Assign the CMD prompt command and launch
@@ -135,10 +167,12 @@ namespace GuildWars2_Launcher
             System.Diagnostics.Process.Start(@"C:\Windows\System32\CMD.exe", loginCommand);
 
             //Update and write the 'LastLogin' time to the XML file
-            this.dt.Rows[currentRowIndex][6] = updatedDate.ToString();
+            this.dt.Rows[currentRowIndex][5] = updatedDate.ToString();
             this.ds.AcceptChanges();
             this.ds.WriteXml(this.XmlDirectory);
+
+            //Set the current row to reload to
+            this.currRow = currentRowIndex;
         }
     }
 }
-
